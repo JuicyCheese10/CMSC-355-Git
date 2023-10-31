ad5374c6c2b09b7f237a202b2a0b2b44d5dba06d
Sterling Glasheen <Sterlingglasheen@gmail.com>
2023-10-30 23:21:26 -0400

    My first unit test

---
+++ test_Add_Negative.py
@@ -0,0 +1,10 @@
+import unittest
+from python_Add import add
+
+class test_Add_Negative(unittest.TestCase):
+
+    def test_add_positive_numbers(self):
+        self.assertEqual(add(-3, -5), -8)
+
+if __name__ == '__main__':
+    unittest.main()
